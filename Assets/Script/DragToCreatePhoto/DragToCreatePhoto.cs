9c465efd6df101ec9fc1d3c7f5b03a093497eec6
kevzn <paledust@hotmail.com>
2026-05-16 14:24:29 +0800

    Update DragToCreatePhoto.cs

--- Assets/Script/DragToCreatePhoto/DragToCreatePhoto.cs
+++ Assets/Script/DragToCreatePhoto/DragToCreatePhoto.cs
@@ -46,20 +46,94 @@ public class DragToCreatePhoto : MonoBehaviour
     [System.Serializable]
     private class EdgeHandler
     {
-        private List<EdgeLine> currentEdges = new List<EdgeLine>();
-        public bool m_hasEdge => currentEdges!=null && currentEdges.Count > 0;
         public struct EdgeLine
         {
-            public Vector2 start;
-            public Vector2 end;
-            public EdgeLine(Vector2 start, Vector2 end)
+            public readonly Vector2 start;
+            public readonly Vector2 end;
+            private readonly EdgeType edgeType;
+
+            public EdgeLine(Vector2 start, Vector2 end, EdgeType edgeType)
             {
                 this.start = start;
                 this.end = end;
+                this.edgeType = edgeType;
+            }
+            public Vector2 PointCorrection(Vector2 startPoint, Vector2 endPoint)
+            {
+                switch(edgeType)
+                {
+                    case EdgeType.Top:
+                        if(startPoint.y < start.y)
+                            return endPoint;
+                        else
+                            return EdgeCorrect(startPoint, endPoint);
+                    case EdgeType.Bottom:
+                        if(startPoint.y > start.y)
+                            return endPoint;
+                        else
+                            return EdgeCorrect(startPoint, endPoint);
+                    case EdgeType.Left:
+                        if(startPoint.x > start.x)
+                            return endPoint;
+                        else
+                            return EdgeCorrect(startPoint, endPoint);
+                    case EdgeType.Right:
+                        if(startPoint.x < start.x)
+                            return endPoint;
+                        else
+                            return EdgeCorrect(startPoint, endPoint);
+                    default:
+                        return endPoint;
+                }
+            }
+            private Vector2 EdgeCorrect(Vector2 startPoint, Vector2 endPoint)
+            {
+                bool isVertical = edgeType == EdgeType.Left || edgeType == EdgeType.Right;
+                if(isVertical)
+                {
+                    if((startPoint.y - start.y) * (endPoint.y - start.y) > 0 && 
+                        (startPoint.y - end.y) * (endPoint.y - end.y) > 0 &&
+                        (start.y - startPoint.y) * (end.y - startPoint.y) > 0)
+                        return endPoint;
+                    else
+                        switch(edgeType)
+                        {
+                            case EdgeType.Left:
+                                endPoint.x = Mathf.Min(endPoint.x, start.x);
+                                return endPoint;
+                            case EdgeType.Right:
+                                endPoint.x = Mathf.Max(endPoint.x, start.x);
+                                return endPoint;
+                            default:
+                                return endPoint;
+                        }
+                }
+                else
+                {
+                    if((startPoint.x - start.x) * (endPoint.x - start.x) > 0 && 
+                        (startPoint.x - end.x) * (endPoint.x - end.x) > 0 &&
+                        (start.x - startPoint.x) * (end.x - startPoint.x) > 0)
+                        return endPoint;
+                    else
+                        switch(edgeType)
+                        {
+                            case EdgeType.Top:
+                                endPoint.y = Mathf.Max(endPoint.y, start.y);
+                                return endPoint;
+                            case EdgeType.Bottom:
+                                endPoint.y = Mathf.Min(endPoint.y, start.y);
+                                return endPoint;
+                            default:
+                                return endPoint;
+                        }
+                }
             }
         }
+        [SerializeField] private GameObject edgePrefab;
+        private List<EdgeLine> currentEdges = new List<EdgeLine>();
 
-        [SerializeField] public GameObject edgePrefab;
+        public List<EdgeLine> m_currentEdges => currentEdges;
+        public bool m_hasEdge => currentEdges!=null && currentEdges.Count > 0;
 
         public EdgeLine AddEdgeToRect(Rect rect, EdgeType edgeType)
         {
@@ -67,19 +141,19 @@ public class DragToCreatePhoto : MonoBehaviour
             switch(edgeType)
             {
                 case EdgeType.Top:
-                    edge = new EdgeLine(rect.min + Vector2.up * rect.height, rect.max);
+                    edge = new EdgeLine(rect.min + Vector2.up * rect.height, rect.max, EdgeType.Top);
                     break;
                 case EdgeType.Bottom:
-                    edge = new EdgeLine(rect.min, rect.max + Vector2.down * rect.height);
+                    edge = new EdgeLine(rect.min, rect.max + Vector2.down * rect.height, EdgeType.Bottom);
                     break;
                 case EdgeType.Left:
-                    edge = new EdgeLine(rect.min, rect.min + Vector2.up * rect.height);
+                    edge = new EdgeLine(rect.min, rect.min + Vector2.up * rect.height, EdgeType.Left);
                     break;
                 case EdgeType.Right:
-                    edge = new EdgeLine(rect.max, rect.max + Vector2.down * rect.height);
+                    edge = new EdgeLine(rect.max + Vector2.down * rect.height, rect.max, EdgeType.Right);
                     break;
                 default:
-                    edge = new EdgeLine(Vector2.zero, Vector2.right);
+                    edge = new EdgeLine(Vector2.zero, Vector2.right, EdgeType.None);
                     break;
             }
             var edgeObj = Instantiate(edgePrefab);
@@ -147,6 +221,14 @@ public class DragToCreatePhoto : MonoBehaviour
             worldPos.z = 0;
             maxPoint = worldPos;
 
+            if(edgeHandler.m_hasEdge)
+            {
+                foreach(var edge in edgeHandler.m_currentEdges)
+                {
+                    maxPoint = edge.PointCorrection(minPoint, maxPoint);
+                }
+            }
+
             var rect = Rect.MinMaxRect(minPoint.x, minPoint.y, maxPoint.x, maxPoint.y);
             if(rect.width > maxSize.x)
                 rect.xMax = rect.xMin + maxSize.x;
@@ -157,13 +239,6 @@ public class DragToCreatePhoto : MonoBehaviour
             else if(rect.height < -maxSize.y)
                 rect.yMax = rect.yMin - maxSize.y;
             
-            Vector2 min = new Vector2(rect.xMin, rect.yMin);
-            Vector2 max = new Vector2(rect.xMax, rect.yMax);
-            rect.min = min;
-            rect.max = max;
-            minPoint = rect.min;
-            maxPoint = rect.max;
-
             currentFrame.UpdateFrame(rect, FRAME_OFFSET);
             maskHandler.UpdateMask(currentFrame.transform.position, rect.size);
         }
@@ -191,6 +266,14 @@ public class DragToCreatePhoto : MonoBehaviour
         if(currentFrame != null)
         {
             var rect = Rect.MinMaxRect(minPoint.x, minPoint.y, maxPoint.x, maxPoint.y);
+            Vector2 min = new Vector2(Mathf.Min(rect.min.x, rect.max.x), Mathf.Min(rect.min.y, rect.max.y));
+            Vector2 max = new Vector2(Mathf.Max(rect.min.x, rect.max.x), Mathf.Max(rect.min.y, rect.max.y));
+            rect.min = min;
+            rect.max = max;
+
+            minPoint = rect.min;
+            maxPoint = rect.max;
+
             if(Mathf.Abs(rect.width) < minSize.x || Mathf.Abs(rect.height) < minSize.y)
             {
                 CancelFrame();
